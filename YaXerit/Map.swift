934cd32d2e4ba0b45fb4d7c103ec96132b76934f
Huseyn Gorbani <gorbanih@uni.coventry.ac.uk>
2020-06-20 12:37:52 +0400

    This View is for showing basic map first.

---
+++ YaXerit/Map.swift
@@ -0,0 +1,21 @@
+//
+//  Map.swift
+//  YaXerit
+//
+//  Created by Huseyn Gorbani on 20/06/2020.
+//  Copyright © 2020 Huseyn Gorbani. All rights reserved.
+//
+
+import SwiftUI
+
+struct Map: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Map_Previews: PreviewProvider {
+    static var previews: some View {
+        Map()
+    }
+}
